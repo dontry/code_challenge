0936ffcb6808b819c1c3f2f1c6373759b7f6b59d
dontry <mccoy018@gmail.com>
2018-07-10 21:17:13 +1000

    added .gitignore; .eslintrc.js;

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard"
-    ]
+  extends: "standard",
+  plugins: ["standard"],
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: "module"
+  },
+  env: {
+    es6: true,
+    node: true
+  }
 };
